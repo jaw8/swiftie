f9b1b63475688db786dfd86357a3ed3fe886e23b
jaw8 <62952761+jaw8@users.noreply.github.com>
2023-10-25 12:11:29 -0400

    ridgeline plot

--- infographs.R
+++ infographs.R
@@ -1,10 +1,33 @@
 library(tidyverse)
 library(taylor)
-library(ggbump)
 library(ggridges)
 library(gt)
 library(gtExtras)
 
+# Albums ----
+cover_links <- c(
+  "Taylor Swift" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-self-titled-billboard-1240.jpg?w=768",
+  "Speak Now" = "https://www.billboard.com/wp-content/uploads/2022/06/taylor-swift-speak-now-billboard-1240.jpg?w=768",
+  "1989" = "https://www.billboard.com/wp-content/uploads/2015/06/taylor-swift-1989-album-billboard-1548.jpg?w=768",
+  "reputation" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-reputation-billboard-1240.jpg?w=1024",
+  "Lover" = "https://www.billboard.com/wp-content/uploads/media/Taylor-Swift-Lover-album-art-2019-billboard-1240.jpg?w=768",
+  "folklore" = "https://www.billboard.com/wp-content/uploads/2020/12/Taylor-swift-folklore-cover-billboard-1240-1607121703.jpg?w=768",
+  "evermore" = "https://www.billboard.com/wp-content/uploads/2020/12/taylor-swift-cover-2020-billboard-1240-1607612466.jpg?w=768",
+  "Fearless (Taylor's Version)" = "https://www.billboard.com/wp-content/uploads/2021/04/Taylor-Swift-fearless-album-art-cr-Beth-Garrabrant-billboard-1240-1617974663.jpg?w=768",
+  "Red (Taylor's Version)" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-red-taylors-version-billboard-1240.jpg?w=768",
+  "Midnights" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-midnights-album-cover-2022-billboard-1240.jpg?w=768"
+)
+
+covers <- image_read(cover_links)
+
+albums_covers <- taylor_albums %>%
+  mutate(art = cover_links[album_name]) %>%
+  filter(ep==F & !is.na(art)) %>%
+  mutate(art = paste('<img src=',art, 'width="20" />'))
+  
+alffd <- paste('<img src=',cover_links, 'width="20" />')
+
+# Songs ----
 View(taylor_album_songs)
 
 df <- taylor_album_songs %>%
@@ -25,21 +48,8 @@ df_pivot_summary <- df_pivot %>%
   summarise(avg = mean(value),
             .by = c(album_name, album_release, param)
             ) 
+
 # Plots ----
-cover_links <- c(
-  "Taylor Swift" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-self-titled-billboard-1240.jpg?w=768",
-  "Speak Now" = "https://www.billboard.com/wp-content/uploads/2022/06/taylor-swift-speak-now-billboard-1240.jpg?w=768",
-  "1989" = "https://www.billboard.com/wp-content/uploads/2015/06/taylor-swift-1989-album-billboard-1548.jpg?w=768",
-  "reputation" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-reputation-billboard-1240.jpg?w=1024",
-  "Lover" = "https://www.billboard.com/wp-content/uploads/media/Taylor-Swift-Lover-album-art-2019-billboard-1240.jpg?w=768",
-  "folklore" = "https://www.billboard.com/wp-content/uploads/2020/12/Taylor-swift-folklore-cover-billboard-1240-1607121703.jpg?w=768",
-  "evermore" = "https://www.billboard.com/wp-content/uploads/2020/12/taylor-swift-cover-2020-billboard-1240-1607612466.jpg?w=768",
-  "Fearless (Taylor's Version)" = "https://www.billboard.com/wp-content/uploads/2021/04/Taylor-Swift-fearless-album-art-cr-Beth-Garrabrant-billboard-1240-1617974663.jpg?w=768",
-  "Red (Taylor's Version)" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-red-taylors-version-billboard-1240.jpg?w=768",
-  "Midnights" = "https://www.billboard.com/wp-content/uploads/2022/10/taylor-swift-midnights-album-cover-2022-billboard-1240.jpg?w=768"
-)
-
-
 p <-
   ggplot(df_pivot,
          aes(
@@ -53,8 +63,13 @@ p <-
   scale_fill_albums() +
   theme_minimal() +
   facet_wrap(vars(param),
-             ncol = 3,
-             scales = 'free_x')
+             nrow = 1,
+             scales = 'free_x') +
+  scale_y_discrete(labels = function(.x)
+    paste0(as.character(.x), "<br>",
+           image_read(alffd[.x]))
+  )
 
 p
 
+gt::gt(p)
